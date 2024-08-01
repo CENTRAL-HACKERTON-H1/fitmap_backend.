c9f29c6b2eb1889458ae8cbf4027109c48086e65
Oesikgogi <leecheol012566@gmail.com>
2024-08-02 01:06:16 +0900

    fix: cors error5

--- community/settings.py
+++ community/settings.py
@@ -80,8 +80,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "http://fitmap.store"
-    "http://fitmap.store/board/"]
+    "https://fitmap.store"
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
