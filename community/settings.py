63438f02672d87a374c04f538bca1a166bb35ec3
Oesikgogi <leecheol012566@gmail.com>
2024-08-01 18:34:38 +0900

    fix: CORS ERROR

--- community/settings.py
+++ community/settings.py
@@ -78,7 +78,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "http://localhost:5173"]
+    "http://localhost:5173",
+    "http://fitmap.store"]
 
 CORS_ALLOW_CREDENTIALS = True
 
