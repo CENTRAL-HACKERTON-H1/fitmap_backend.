9d31d28233696ce2690d776b0e7e53891f162af5
Oesikgogi <leecheol012566@gmail.com>
2024-08-01 18:23:49 +0900

    return: debug False

--- community/settings.py
+++ community/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['43.201.162.111', 'fitmape.store', '127.0.0.1']
+ALLOWED_HOSTS = ['43.201.162.111', 'fitmap.store', '127.0.0.1']
 
 
 # Application definition
