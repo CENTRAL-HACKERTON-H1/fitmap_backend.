576970538db00e8597b2f8062431c92db517a87b
Oesikgogi <leecheol012566@gmail.com>
2024-08-05 19:01:43 +0900

    fix: url update

--- community/settings.py
+++ community/settings.py
@@ -80,7 +80,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "https://fitmap.store"
+    "https://fitmap.store",
+    "https://main--fitmapstore.netlify.app/"
     ]
 
 CORS_ALLOW_CREDENTIALS = True
